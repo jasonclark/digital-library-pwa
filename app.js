13f1ce46dfb6cd996264bcad5c17990d4337aff4
jasonclark <jasclark@gmail.com>
2018-12-02 15:51:30 -0700

    removed getQueryVariable from global scope of app.js

--- app.js
+++ app.js
@@ -45,20 +45,6 @@ class DigLibApp {
     observer.observe(header);
   }
 
-  getQueryVariable(variable) {
-    const query = window.location.search.substring(1);
-    const vars = query.split('&');
-
-    for (let i = 0; i < vars.length; i++) {
-      const pair = vars[i].split('=');
-
-      if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-      }
-    }
-  }
-  //const searchTerm = getQueryVariable('query');
-
   async loadFeatured() {
     this.featured = await this.fetchJSON('./items.json');
 
